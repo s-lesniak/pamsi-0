1b959ae24e5643ce5c9d8b503ff1a3741b053331
Szymon Leśniak <200389@student.pwr.edu.pl>
2014-03-02 22:16:18 +0100

    SprawdzNazwe

--- src/eksperyment.cpp
+++ src/eksperyment.cpp
@@ -1,10 +1,23 @@
 /*! @file eksperyment.cpp
  * @brief Definicje metod klasy Eksperyment */
 #include <iostream>
+#include <fstream>
 
 #include "eksperyment.hh"
 
 using namespace std;
+
+bool SprawdzNazwe(string nazwa)
+{
+  ifstream str(nazwa.c_str());
+
+  if (!(str.is_open())) {
+    cerr << "Pliku " << nazwa << "nie udało się otworzyć" << endl;
+    return false;
+  }
+  else
+    return true;
+}
 
 Eksperyment::Eksperyment(string PlikWyj): NazwaWyjscia(PlikWyj)
 {
@@ -19,7 +32,7 @@ bool Eksperyment::WczytajPliki (unsigned nr)
 
 bool Eksperyment::SpiszZadania ()
 {
-  unsigned n;
+  unsigned n, k;
   cout << "Podaj liczbę plików wejściowych: ";
   cin >> n;
   if (cin.fail()) {
@@ -28,6 +41,18 @@ bool Eksperyment::SpiszZadania ()
   }
   Zadania.resize(n);
 
-  for (int i = 0; i < n; i++) {
-    
+  for (unsigned i = 0; i < n; i++) {
+    cout << "Podaj plik źródłowy nr " << (i+1) << ": "; 
+    if (!(cin >> Zadania[i].PlikWejsciowy)) return false;
+    if (!SprawdzNazwe(Zadania[i].PlikWejsciowy)) return false;
+    cout << "Podaj plik wzorcowy nr " << (i+1) << ": ";
+    if (!(cin >> Zadania[i].PlikWzorcowy)) return false;
+    if (!SprawdzNazwe(Zadania[i].PlikWzorcowy)) return false;
+    cout << "Ilość badań pliku nr " << (i+1) << ": ";
+    cin >> Zadania[i].IleRazy;
+    if (cin.fail()) {
+      cerr << "To nie jest liczba" << endl;
+      return false;
+    }
+  }
 }
