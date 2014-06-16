501d5c4a31cbf3bb4a08749430f3904da9d6bfcd
Szymon Leśniak <200389@student.pwr.edu.pl>
2014-06-16 20:57:38 +0200

    Revert

--- src/graf.cpp
+++ src/graf.cpp
@@ -141,6 +141,7 @@ vector<unsigned> Graf<Wezel_t, Koszt_t>::BFS
 	unsigned i;
 	kolejka.push(start);
 	V.push_back(start);
+	licznik++;
 
 	while (!kolejka.empty()) {
 		i = kolejka.front();
@@ -153,6 +154,7 @@ vector<unsigned> Graf<Wezel_t, Koszt_t>::BFS
 			if (!count(V.begin(), V.end(), k) ) {
 				V.push_back(k);
 				kolejka.push(k);
+				licznik++;
 				ojcowie[k] = i;
 			}
 		}
@@ -179,6 +181,7 @@ vector<unsigned> Graf<Wezel_t, Koszt_t>::DFS
 
 	stos.push(start);
 	V.push_back(start);
+	licznik++;
 
 	while(!stos.empty()) {
 		i = stos.top();
@@ -191,6 +194,7 @@ vector<unsigned> Graf<Wezel_t, Koszt_t>::DFS
 			if (!count(V.begin(), V.end(), k) ) {
 				V.push_back(k);
 				stos.push(k);
+				licznik++;
 				ojcowie[k] = i;
 			}
 		}
@@ -215,10 +219,12 @@ vector<unsigned> Graf<Wezel_t, Koszt_t>::AStar
 	map<unsigned, unsigned> ojcowie;
 
 	odwiedzone.insert(start);
+	licznik++;
 	vector<unsigned> sasiedzi = NrySasiadow(start);
 	for (unsigned j = 0; j < sasiedzi.size(); j++) {
 		AStar_elem n_elem (sasiedzi[j], Odleglosc(sasiedzi[j], start));
 		badane.insert(n_elem);
+		licznik++;
 		if (!ojcowie.count(n_elem.nr))
 			ojcowie[n_elem.nr] = start;
 	}
@@ -247,6 +253,7 @@ vector<unsigned> Graf<Wezel_t, Koszt_t>::AStar
 					Odleglosc(sasiedzi[j], i.nr));
 			if (!ojcowie.count(n_elem.nr)) {
 				badane.insert(n_elem);
+				licznik++;
 				ojcowie[n_elem.nr] = i.nr;
 			}
 		}
